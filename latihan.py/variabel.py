52d7e403f451a290f2e0d4bcea7531f5ac0dfa37
davidputra25 <davidalif018@gmail.com>
2025-11-21 12:41:42 +0700

    variabel.py

---
+++ latihan.py/variabel.py
@@ -0,0 +1,11 @@
+print('---Belajar Variabel---') 
+char = 'B' 
+string = "david alif tiano putra" 
+a = 19
+penghasilan1 = 25000000 
+nilai_uts = 82.25 
+print(char) 
+print(string) 
+print(a) 
+print(penghasilan1) 
+print(nilai_uts) 
