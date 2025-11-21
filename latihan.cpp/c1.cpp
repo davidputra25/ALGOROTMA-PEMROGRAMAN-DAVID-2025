791646d36432b45c6093490d7a583cb65863276b
davidputra25 <davidalif018@gmail.com>
2025-11-21 11:23:04 +0700

    penugasan pada matkul algoritma dan pemrograman

---
+++ latihan.cpp/c1.cpp
@@ -0,0 +1,15 @@
+include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "david";
+    int umur = 19;
+    float tinggi = 180;
+    bool aktif = true;
+
+    cout << "Nama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+    cout << "Tinggi: " << tinggi << endl;
+    cout << "Status aktif: " << (aktif ? "true" : "false") << endl;
+    return 0;
+}
