f438f0121a0877a81d7ee085245a02d88581bc6a
davidputra25 <davidalif018@gmail.com>
2026-01-31 16:38:30 +0700

    Add files via upload

---
+++ tugas-prosedur-fungsi/prosedur_fungsi.py
@@ -0,0 +1,17 @@
+# Contoh Prosedur
+def tampilkan_salam():
+    print("Halo, ini adalah contoh prosedur")
+
+# Contoh Fungsi
+def hitung_luas_persegi(sisi):
+    return sisi * sisi
+
+
+# Pemanggilan Prosedur
+tampilkan_salam()
+
+print()  # spasi baris
+
+# Pemanggilan Fungsi
+luas = hitung_luas_persegi(5)
+print("Luas persegi adalah:", luas)
