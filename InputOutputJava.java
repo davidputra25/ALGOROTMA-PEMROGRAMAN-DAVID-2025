f5686b4913070e38ad424f34c82c8d69bff38df8
davidputra25 <davidalif018@gmail.com>
2026-01-31 16:15:41 +0700

    Add files via upload

---
+++ InputOutputJava.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class InputOutputJava {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nama: ");
+        String nama = input.nextLine();
+
+        System.out.print("Masukkan umur: ");
+        int umur = input.nextInt();
+
+        System.out.println("Halo " + nama);
+        System.out.println("Umur kamu " + umur + " tahun");
+
+        input.close();
+    }
+}
---
+++ input_output.py
@@ -0,0 +1,7 @@
+# Input
+nama = input("Masukkan nama: ")
+umur = input("Masukkan umur: ")
+
+# Output
+print("Halo", nama)
+print("Umur kamu", umur, "tahun")
