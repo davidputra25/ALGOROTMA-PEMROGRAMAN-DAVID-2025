9909e91608e4458819f927bcacef3d57ecbb4f57
davidputra25 <davidalif018@gmail.com>
2026-01-31 16:33:54 +0700

    Add files via upload

---
+++ tugas-perulangan-array/perulangan_array.py
@@ -0,0 +1,9 @@
+# Contoh Perulangan
+for i in range(1, 6):
+    print("Perulangan ke-", i)
+
+print()  # spasi baris
+
+# Contoh Array
+nilai = [80, 85, 90, 75, 88]
+print("Isi array nilai:", nilai)
